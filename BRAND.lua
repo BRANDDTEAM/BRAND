5b4c8b2ae4a323c30aef32f43a92fbbc8d6de651
BRANDDTEAM <79874762+BRANDDTEAM@users.noreply.github.com>
2021-05-30 21:37:00 +0300

    Add files via upload

--- BRAND.lua
+++ BRAND.lua
@@ -5237,25 +5237,6 @@ end
 tdcli_function ({ID = "SearchPublicChat",username_ = username}, start_function, nil)
 return false
 end  
-if text == "ترتيب الاوامر" and Constructor(msg) then
-database:set(bot_id.."BRAND:Set:Cmd:Group:New1"..msg.chat_id_..":ا","ايدي")
-database:sadd(bot_id.."BRAND:List:Cmd:Group:New"..msg.chat_id_,"ا")
-database:set(bot_id.."BRAND:Set:Cmd:Group:New1"..msg.chat_id_..":م","رفع مميز")
-database:sadd(bot_id.."BRAND:List:Cmd:Group:New"..msg.chat_id_,"م")
-database:set(bot_id.."BRAND:Set:Cmd:Group:New1"..msg.chat_id_..":اد","رفع ادمن")
-database:sadd(bot_id.."BRAND:List:Cmd:Group:New"..msg.chat_id_,"اد")
-database:set(bot_id.."BRAND:Set:Cmd:Group:New1"..msg.chat_id_..":مد","رفع مدير")
-database:sadd(bot_id.."BRAND:List:Cmd:Group:New"..msg.chat_id_,"مد")
-database:set(bot_id.."BRAND:Set:Cmd:Group:New1"..msg.chat_id_..":من","رفع منشئ")
-database:sadd(bot_id.."BRAND:List:Cmd:Group:New"..msg.chat_id_,"من")
-database:set(bot_id.."BRAND:Set:Cmd:Group:New1"..msg.chat_id_..":اس","رفع منشئ اساسي")
-database:sadd(bot_id.."BRAND:List:Cmd:Group:New"..msg.chat_id_,"اس")
-database:set(bot_id.."BRAND:Set:Cmd:Group:New1"..msg.chat_id_..":تعط","تعطيل الايدي بالصوره")
-database:sadd(bot_id.."BRAND:List:Cmd:Group:New"..msg.chat_id_,"تعط")
-database:set(bot_id.."BRAND:Set:Cmd:Group:New1"..msg.chat_id_..":تفع","تفعيل الايدي بالصوره")
-database:sadd(bot_id.."BRAND:List:Cmd:Group:New"..msg.chat_id_,"تفع")
-send(msg.chat_id_, msg.id_,"☆:تم ترتيب الاوامر بالشكل التالي ~\n- ايدي - ا .\n- مميز - م .\n- ادمن - اد .\n- مدير - مد . \n- منشى - من . \n- المنشئ الاساسي - اس  . \n- تعطيل الايدي بالصوره - تعط .\n- تفعيل الايدي بالصوره - تفع .")  
-end
 if text and text:match("^طرد (%d+)$") and Mod(msg) then 
 local userid = text:match("^طرد (%d+)$") 
 if AddChannel(msg.sender_user_id_) == false then
@@ -5398,7 +5379,25 @@ end
 tdcli_function ({ID = "SearchPublicChat",username_ = username}, start_function, nil)
 return false
 end
-
+if text == "ترتيب الاوامر" and Constructor(msg) then
+database:set(bot_id.."VVVZVV:Set:Cmd:Group:New1"..msg.chat_id_..":ا","ايدي")
+database:sadd(bot_id.."VVVZVV:List:Cmd:Group:New"..msg.chat_id_,"ا")
+database:set(bot_id.."VVVZVV:Set:Cmd:Group:New1"..msg.chat_id_..":م","رفع مميز")
+database:sadd(bot_id.."VVVZVV:List:Cmd:Group:New"..msg.chat_id_,"م")
+database:set(bot_id.."VVVZVV:Set:Cmd:Group:New1"..msg.chat_id_..":اد","رفع ادمن")
+database:sadd(bot_id.."VVVZVV:List:Cmd:Group:New"..msg.chat_id_,"اد")
+database:set(bot_id.."VVVZVV:Set:Cmd:Group:New1"..msg.chat_id_..":مد","رفع مدير")
+database:sadd(bot_id.."VVVZVV:List:Cmd:Group:New"..msg.chat_id_,"مد")
+database:set(bot_id.."VVVZVV:Set:Cmd:Group:New1"..msg.chat_id_..":من","رفع منشئ")
+database:sadd(bot_id.."VVVZVV:List:Cmd:Group:New"..msg.chat_id_,"من")
+database:set(bot_id.."VVVZVV:Set:Cmd:Group:New1"..msg.chat_id_..":اس","رفع منشئ اساسي")
+database:sadd(bot_id.."VVVZVV:List:Cmd:Group:New"..msg.chat_id_,"اس")
+database:set(bot_id.."VVVZVV:Set:Cmd:Group:New1"..msg.chat_id_..":تعط","تعطيل الايدي بالصوره")
+database:sadd(bot_id.."VVVZVV:List:Cmd:Group:New"..msg.chat_id_,"تعط")
+database:set(bot_id.."VVVZVV:Set:Cmd:Group:New1"..msg.chat_id_..":تفع","تفعيل الايدي بالصوره")
+database:sadd(bot_id.."VVVZVV:List:Cmd:Group:New"..msg.chat_id_,"تفع")
+send(msg.chat_id_, msg.id_,"☆:تم ترتيب الاوامر بالشكل التالي ~\n- ايدي - ا .\n- مميز - م .\n- ادمن - اد .\n- مدير - مد . \n- منشى - من . \n- المنشئ الاساسي - اس  . \n- تعطيل الايدي بالصوره - تعط .\n- تفعيل الايدي بالصوره - تفع .")  
+end
 if text and text:match("^رفع مميز (%d+)$") and Mod(msg) then
 local userid = text:match("^رفع مميز (%d+)$")
 if AddChannel(msg.sender_user_id_) == false then
